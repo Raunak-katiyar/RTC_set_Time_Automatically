7157531fd0bb3b0e3569fd3a07cff94d411b2cce
Raunak katiyar <katiyarraunak2468@gmail.com>
2025-11-08 11:48:44 +0530

    RTC Time Set

---
+++ RTC_set_Time_Automatically.ino
@@ -0,0 +1,23 @@
+#include <Wire.h>
+#include <RTClib.h>
+
+RTC_DS3231 rtc;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  
+  Wire.begin(21, 22);  // Your I2C pins
+  
+  if (!rtc.begin()) {
+    Serial.println("RTC not found!");
+    while (1);
+  }
+
+  // Adjust the time once to the current PC time
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); 
+
+  Serial.println("RTC time set to compile time.");
+}
+
+void loop() {}
